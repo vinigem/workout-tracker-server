1afe9725bcf019c15bed0078061527c7820344f6
Vinit Kumar <Kumar_Vinit@cat.com>
2018-03-22 16:46:15 +0530

    added cors mapping config

--- src/main/java/com/vini/workouttracker/Application.java
+++ src/main/java/com/vini/workouttracker/Application.java
@@ -1,15 +1,29 @@
 package com.vini.workouttracker;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@Configuration
 public class Application {
 	
 	public static void main(final String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 	
 }
