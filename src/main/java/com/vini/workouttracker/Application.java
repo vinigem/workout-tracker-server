4926e98d33fe835added29d777a26f0b984158cd
Vinit Kumar <Kumar_Vinit@cat.com>
2018-04-02 18:38:11 +0530

    modified spring security config

--- src/main/java/com/vini/workouttracker/Application.java
+++ src/main/java/com/vini/workouttracker/Application.java
@@ -2,8 +2,12 @@ package com.vini.workouttracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.vini.workouttracker.security.SecurityConfig;
 
 @SpringBootApplication
+@Import({SecurityConfig.class})
 public class Application {
 	
 	public static void main(final String[] args) {
