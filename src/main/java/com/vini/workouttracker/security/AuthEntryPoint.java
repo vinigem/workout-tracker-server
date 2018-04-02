9b5d7e993095d367c57244e0d696cb7c0b02a665
Vinit Kumar <Kumar_Vinit@cat.com>
2018-04-02 18:30:13 +0530

    added user authentication

--- src/main/java/com/vini/workouttracker/security/AuthEntryPoint.java
+++ src/main/java/com/vini/workouttracker/security/AuthEntryPoint.java
@@ -9,7 +9,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AuthEntryPoint extends BasicAuthenticationEntryPoint {
 
 	@Override
