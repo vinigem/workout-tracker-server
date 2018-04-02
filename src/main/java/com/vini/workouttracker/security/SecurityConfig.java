153838369b2126f11f3379e9c57fdbbb71076e57
Vinit Kumar <Kumar_Vinit@cat.com>
2018-04-02 17:58:21 +0530

    added spring security config

--- src/main/java/com/vini/workouttracker/security/SecurityConfig.java
+++ src/main/java/com/vini/workouttracker/security/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.vini.workouttracker.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -64,5 +66,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.and()
 			.csrf().disable();
 	}
+	
+	/* To allow Pre-flight [OPTIONS] request from browser */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+    }
 
 }
