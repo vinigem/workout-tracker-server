51514bb515acc4cc4c0d9c1b8e1831e8cdcb2936
Vinit Kumar <Kumar_Vinit@cat.com>
2018-04-03 13:43:39 +0530

    modified spring security config

--- src/main/java/com/vini/workouttracker/Application.java
+++ src/main/java/com/vini/workouttracker/Application.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.vini.workouttracker.security.SecurityConfig;
+
 @SpringBootApplication
 @Configuration
+@Import({SecurityConfig.class})
 public class Application {
 	
 	public static void main(final String[] args) {
