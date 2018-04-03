f72829334fa215534d3cddee59f714f8695ad923
Vinit Kumar <Kumar_Vinit@cat.com>
2018-04-03 11:55:13 +0530

    modified spring security config

--- src/main/java/com/vini/workouttracker/security/AuthEntryPoint.java
+++ src/main/java/com/vini/workouttracker/security/AuthEntryPoint.java
@@ -1,26 +1,27 @@
 package com.vini.workouttracker.security;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthEntryPoint extends BasicAuthenticationEntryPoint {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(AuthEntryPoint.class);
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
 			throws IOException, ServletException {
 		response.addHeader("WWW-Authenticate", "Basic realm=\"" + getRealmName() + "\"");
 		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-		PrintWriter writer = response.getWriter();
-		writer.println("HTTP Status 401 - " + authEx.getMessage());
+		LOGGER.error("HTTP Status 401 - {}", authEx.getMessage());
 	}
 
 	@Override
