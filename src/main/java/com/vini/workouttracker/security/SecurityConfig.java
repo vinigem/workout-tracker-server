0f5c90c9bf4687877df37ae6718379a6bf8ee22f
Vinit Kumar <Kumar_Vinit@cat.com>
2018-04-03 11:09:43 +0530

    modified spring security config

--- src/main/java/com/vini/workouttracker/security/SecurityConfig.java
+++ src/main/java/com/vini/workouttracker/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.vini.workouttracker.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +15,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import com.vini.workouttracker.model.User;
 import com.vini.workouttracker.repository.IUserDAO;
 
@@ -51,7 +56,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
+		http.cors()
+		.and()
 			.authorizeRequests()
 			.antMatchers("/login", "/register").permitAll()
 			.anyRequest().fullyAuthenticated()
@@ -66,5 +72,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.and()
 			.csrf().disable();			
 	}
-			
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "HEAD", "PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("*"));
+		configuration.setAllowCredentials(true);
+		configuration.setMaxAge(1800L);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+				
 }
